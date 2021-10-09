141a4380a612a564b2fb705d156af24bb908c6f8
HyunSung-Na <oceana57@naver.com>
2021-10-09 20:45:02 +0900

    feat: RacingTrack findWinnerCars fun add

--- src/main/java/racinggame/game/RacingTrack.java
+++ src/main/java/racinggame/game/RacingTrack.java
@@ -1,7 +1,11 @@
 package racinggame.game;
 
+import racinggame.game.domain.Car;
 import racinggame.game.domain.Cars;
 import racinggame.game.domain.Round;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RacingTrack {
 
@@ -9,6 +13,7 @@ public class RacingTrack {
     private final int MIN_CAR_LENGTH = 2;
 
     private final Cars cars = new Cars();
+    private final RacingResultView racingResultView = GameFactory.racingResultView();
 
     public void readyStartingLineCars(String userCarNameInput) {
         String[] carNames = userCarNameInput.split(",");
@@ -24,8 +29,25 @@ public class RacingTrack {
     }
 
     public void play(Round round) {
+        racingResultView.gameResultMessage();
+
         for (int roundNumber = 0; roundNumber < round.getRound(); roundNumber++) {
             cars.racing();
+            racingResultView.gameRoundResultMessage(cars.getCars());
+            System.out.println();
         }
+        racingResultView.gameWinnerResultMessage(findWinnerCars());
+    }
+
+    private List<Car> findWinnerCars() {
+        int maxDistance = cars.findMaxDistance();
+        ArrayList<Car> winnerCars = new ArrayList<>();
+
+        for (Car car : cars.getCars()) {
+            if (car.getDistance() == maxDistance) {
+                winnerCars.add(car);
+            }
+        }
+        return winnerCars;
     }
 }
