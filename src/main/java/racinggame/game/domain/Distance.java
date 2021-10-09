4af56aa6f40273c4a480c61b5ff0e4d617265e89
HyunSung-Na <oceana57@naver.com>
2021-10-09 15:16:58 +0900

    feat: Distance Vo Class add

---
+++ src/main/java/racinggame/game/domain/Distance.java
@@ -0,0 +1,14 @@
+package racinggame.game.domain;
+
+public class Distance {
+
+    private int distance = 0;
+
+    public void plusDistance() {
+        distance++;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
