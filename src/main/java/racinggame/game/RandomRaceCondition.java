c3cbc2e490b0e495f7cccdd5dc42a5620e5f0e46
HyunSung-Na <oceana57@naver.com>
2021-10-09 15:49:00 +0900

    feat: RandomRaceCondition class add

---
+++ src/main/java/racinggame/game/RandomRaceCondition.java
@@ -0,0 +1,19 @@
+package racinggame.game;
+
+import nextstep.utils.Randoms;
+
+public class RandomRaceCondition implements RaceCondition{
+    private final int RANDOM_MIN_NUMBER = 0;
+    private final int RANDOM_MAX_NUMBER = 9;
+    private final int MOVE_CONDITION = 4;
+
+    @Override
+    public boolean isMovedCar() {
+        int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER);
+
+        if (randomNumber >= MOVE_CONDITION) {
+            return true;
+        }
+        return false;
+    }
+}
