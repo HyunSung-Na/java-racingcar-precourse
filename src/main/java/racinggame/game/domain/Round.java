d3db43e1ab5a56f14f5ddf7086238586f53aa266
HyunSung-Na <oceana57@naver.com>
2021-10-09 15:18:21 +0900

    feat: Round Vo Class add

---
+++ src/main/java/racinggame/game/domain/Round.java
@@ -0,0 +1,24 @@
+package racinggame.game.domain;
+
+public class Round {
+
+    private final String WRONG_INPUT_MIN_ROUND_MESSAGE = "[ERROR]시도할 회수는 1 이상 입력해주세요.";
+    private final int MIN_ROUND_NUMBER = 1;
+
+    private final int round;
+
+    public Round(int round) {
+        validateRound(round);
+        this.round = round;
+    }
+
+    private void validateRound(int round) {
+        if (round < MIN_ROUND_NUMBER) {
+            throw new IllegalArgumentException(WRONG_INPUT_MIN_ROUND_MESSAGE);
+        }
+    }
+
+    public int getRound() {
+        return round;
+    }
+}
